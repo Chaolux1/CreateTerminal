7ced62023b22787b660f453d9fbf92c448ef424e
Chaolux1 <anothersprojects@gmail.com>
2025-05-25 08:20:34 +0100

    Start

---
+++ src/main/java/net/chaolux/createterminal/common/item/RemoteTerminalItem.java
@@ -0,0 +1,9 @@
+package net.chaolux.createterminal.common.item;
+
+import net.minecraft.world.item.Item;
+
+public class RemoteTerminalItem extends Item {
+    public RemoteTerminalItem(Properties p_41383_) {
+        super(p_41383_);
+    }
+}
---
+++ src/main/java/net/chaolux/createterminal/registry/item/ModItems.java
@@ -0,0 +1,28 @@
+package net.chaolux.createterminal.registry.item;
+
+import net.chaolux.createterminal.common.item.RemoteTerminalItem;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.function.Supplier;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS;
+    public static final RegistryObject<Item> REMOTE_TERMINAL;
+
+    public static RegistryObject<Item> registerWithTab(String name, Supplier<Item> supplier) {
+        return ITEMS.register(name, supplier);
+    }
+
+    public static Item.Properties basicItem() {
+        return new Item.Properties();
+    }
+
+    static {
+        ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, "createcardboardthings");
+
+        REMOTE_TERMINAL = registerWithTab("remote_terminal", () -> new RemoteTerminalItem(basicItem()));
+    }
+}
