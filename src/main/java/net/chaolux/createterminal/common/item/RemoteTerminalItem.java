fb31593abb2edc695b99ba676214758e88642b30
Chaolux1 <anothersprojects@gmail.com>
2025-06-01 09:07:45 +0100

    add RemoteStockKeeperMenu, RemoteTerminalItem

--- src/main/java/net/chaolux/createterminal/common/item/RemoteTerminalItem.java
+++ src/main/java/net/chaolux/createterminal/common/item/RemoteTerminalItem.java
@@ -1,9 +1,91 @@
 package net.chaolux.createterminal.common.item;
 
+import com.simibubi.create.AllMenuTypes;
+import com.simibubi.create.content.logistics.stockTicker.StockKeeperRequestMenu;
+import com.simibubi.create.content.logistics.stockTicker.StockTickerBlockEntity;
+import io.netty.buffer.Unpooled;
+import net.chaolux.createterminal.common.menu.RemoteStockKeeperMenu;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.*;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraftforge.network.NetworkHooks;
+import net.minecraftforge.registries.ForgeRegistries;
+
 
 public class RemoteTerminalItem extends Item {
     public RemoteTerminalItem(Properties p_41383_) {
         super(p_41383_);
+    }
+
+    @Override
+    public InteractionResult useOn(UseOnContext ctx) {
+        Level level=ctx.getLevel();
+        BlockPos pos=ctx.getClickedPos();
+        BlockEntity be=level.getBlockEntity(pos);
+        if(!(be instanceof StockTickerBlockEntity)) return InteractionResult.PASS;
+        ItemStack stack=ctx.getItemInHand();
+        CompoundTag tag=stack.getOrCreateTag();
+        tag.putLong("boundPos",pos.asLong());
+        tag.putString("boundDim",level.dimension().location().toString());
+        if(!level.isClientSide) {
+            ctx.getPlayer().displayClientMessage(Component.literal("Bound at " +pos),true);
+        }
+        return InteractionResult.SUCCESS;
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
+        if(!level.isClientSide) {
+            ItemStack stack=player.getItemInHand(hand);
+            CompoundTag tag=stack.getTag();
+            if(tag==null || !tag.contains("boundPos") || !tag.contains("boundDim")) {
+                player.displayClientMessage(Component.literal("[debug] Not bound"),true);
+                return InteractionResultHolder.pass(stack);
+            }
+            BlockPos pos=BlockPos.of(tag.getLong("boundPos"));
+            String dimId=tag.getString("boundDim");
+            ResourceKey<Level> dimKey=ResourceKey.create(Registries.DIMENSION, new ResourceLocation(dimId));
+            player.displayClientMessage(Component.literal("[debug] boundPos:"+pos+",BoundDim:"+dimId),true);
+            player.displayClientMessage(Component.literal("[debug] CurrentDim:"+level.dimension().location()),true);
+            if(!level.dimension().equals(dimKey)) {
+                player.displayClientMessage(Component.literal("[debug] Wrong dim"), true);
+                return InteractionResultHolder.fail(stack);
+            }
+            if(!level.hasChunkAt(pos)) {
+                player.displayClientMessage(Component.literal("[debug] Chunk pos not loded"),true);
+            }
+            MenuType<?> menuType=ForgeRegistries.MENU_TYPES.getValue(new ResourceLocation("create", "stock_keeper_request"));
+            if(menuType==null) {
+                player.displayClientMessage(Component.literal("[debug] MenuType not find"),true);
+                return InteractionResultHolder.fail(stack);
+            }
+            FriendlyByteBuf menuBuf=new FriendlyByteBuf(Unpooled.buffer());
+            menuBuf.writeBoolean(false);
+            menuBuf.writeBoolean(false);
+            menuBuf.writeBlockPos(pos);
+            MenuProvider provider=new SimpleMenuProvider((id,inv,ply)->new RemoteStockKeeperMenu(menuType, id,inv,menuBuf),Component.literal("Stock Keeper"));
+            player.displayClientMessage(Component.literal("[debug] Opening"),true);
+            NetworkHooks.openScreen((ServerPlayer) player,provider,data-> {
+                data.writeBoolean(false);
+                data.writeBoolean(false);
+                data.writeBlockPos(pos);
+            });
+            return InteractionResultHolder.success(stack);
+        }
+        return InteractionResultHolder.pass(player.getItemInHand(hand));
     }
 }
---
+++ src/main/java/net/chaolux/createterminal/common/menu/RemoteStockKeeperMenu.java
@@ -0,0 +1,24 @@
+package net.chaolux.createterminal.common.menu;
+
+import com.simibubi.create.content.logistics.stockTicker.StockKeeperRequestMenu;
+import com.simibubi.create.content.logistics.stockTicker.StockTickerBlockEntity;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.MenuType;
+
+public class RemoteStockKeeperMenu extends StockKeeperRequestMenu {
+
+    public RemoteStockKeeperMenu(MenuType<?> type, int id, Inventory inv, FriendlyByteBuf extraData) {
+        super(type, id, inv, extraData);
+    }
+
+    public RemoteStockKeeperMenu(MenuType<?> type, int id, Inventory inv, StockTickerBlockEntity contentHolder) {
+        super(type, id, inv, contentHolder);
+    }
+
+    @Override
+    public boolean stillValid(Player player) {
+        return true;
+    }
+}
